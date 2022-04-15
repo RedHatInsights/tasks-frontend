6b53c0720572ce41c615c67c178299a0872224af
Michael Johnson <micjohns@newbeginnings.usersys.redhat.com>
2022-04-15 17:18:08 -0400

    Create app

--- src/DevEntry.js
+++ src/DevEntry.js
@@ -3,19 +3,16 @@ import { BrowserRouter as Router } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { init } from './store';
 import App from './App';
-import { getBaseName } from '@redhat-cloud-services/frontend-components-utilities/helpers';
 import logger from 'redux-logger';
 
-const Tasks = () => (
-  <Provider
-    store={init(
-      ...(process.env.NODE_ENV !== 'production' ? [logger] : [])
-    ).getStore()}
-  >
+import getBaseName from './Utilities/getBaseName';
+
+const TasksDev = () => (
+  <Provider store={init(logger).getStore()}>
     <Router basename={getBaseName(window.location.pathname)}>
       <App />
     </Router>
   </Provider>
 );
 
-export default Tasks;
+export default TasksDev;
--- src/bootstrap.js
+++ src/bootstrap.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import AppEntry from './AppEntry';
+import Tasks from './AppEntry';
 
 const root = document.getElementById('root');
 
-ReactDOM.render(<AppEntry />, root, () =>
+ReactDOM.render(<Tasks />, root, () =>
   root.setAttribute('data-ouia-safe', true)
 );

43a50d18c1a338a5788257c6e5b0c29eb24fd05a
Michael Johnson <micjohns@newbeginnings.usersys.redhat.com>
2023-11-14 15:22:17 -0500

    Add severity mapping for conversion tasks
    Test this on insights-qa account on /tasks/executed/6009. It will fail in stage but pass on this PR.

--- src/SmartComponents/CompletedTaskDetails/TaskEntries.js
+++ src/SmartComponents/CompletedTaskDetails/TaskEntries.js
@@ -5,6 +5,83 @@ import {
 } from '@patternfly/react-icons';
 import React from 'react';
 
+const converttorhelconversionSeverityMap = {
+  error: {
+    text: 'Inhibitor',
+    icon: <ExclamationCircleIcon color="#C9190B" />,
+    titleColor: '#A30000',
+    severityColor: 'red',
+    severityLevel: 2500,
+  },
+  overridable: {
+    text: 'Overridable',
+    icon: <ExclamationCircleIcon color="#C9190B" />,
+    titleColor: '#A30000',
+    severityColor: 'red',
+    severityLevel: 2000,
+  },
+  skip: {
+    text: 'Skipped',
+    icon: <ExclamationCircleIcon color="#C9190B" />,
+    titleColor: '#A30000',
+    severityColor: 'red',
+    severityLevel: 1500,
+  },
+  warning: {
+    text: 'Warning',
+    icon: <ExclamationTriangleIcon color="#F0AB00" />,
+    titleColor: '#795000',
+    severityColor: 'orange',
+    severityLevel: 1000,
+  },
+  info: {
+    text: 'Info',
+    icon: <InfoCircleIcon color="#2B9AF3" />,
+    titleColor: '#002952',
+    severityColor: 'blue',
+    severityLevel: 500,
+  },
+};
+
+// constant to be removed
+const converttorhelconversionstageSeverityMap = {
+  error: {
+    text: 'Inhibitor',
+    icon: <ExclamationCircleIcon color="#C9190B" />,
+    titleColor: '#A30000',
+    severityColor: 'red',
+    severityLevel: 2500,
+  },
+  overridable: {
+    text: 'Overridable',
+    icon: <ExclamationCircleIcon color="#C9190B" />,
+    titleColor: '#A30000',
+    severityColor: 'red',
+    severityLevel: 2000,
+  },
+  skip: {
+    text: 'Skipped',
+    icon: <ExclamationCircleIcon color="#C9190B" />,
+    titleColor: '#A30000',
+    severityColor: 'red',
+    severityLevel: 1500,
+  },
+  warning: {
+    text: 'Warning',
+    icon: <ExclamationTriangleIcon color="#F0AB00" />,
+    titleColor: '#795000',
+    severityColor: 'orange',
+    severityLevel: 1000,
+  },
+  info: {
+    text: 'Info',
+    icon: <InfoCircleIcon color="#2B9AF3" />,
+    titleColor: '#002952',
+    severityColor: 'blue',
+    severityLevel: 500,
+  },
+};
+
 const converttorhelpreanalysisSeverityMap = {
   error: {
     text: 'Inhibitor',
@@ -176,6 +253,8 @@ const leappupgradestageSeverityMap = {
 };
 
 export default {
+  converttorhelconversionSeverityMap,
+  converttorhelconversionstageSeverityMap,
   converttorhelpreanalysisSeverityMap,
   converttorhelpreanalysisstageSeverityMap,
   leapppreupgradeSeverityMap,

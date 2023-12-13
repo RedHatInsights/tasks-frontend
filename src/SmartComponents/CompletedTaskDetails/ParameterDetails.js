7c6bf630d7c4209625ff55dcbdcda9183d301fe2
Michael Johnson <johnsonm325@gmail.com>
2023-12-13 13:59:02 -0500

    feat(RHINENG-6101): Add parameter details to CompletedTaskDetails (#138)
    * feat(RHINENG-6101): Add parameter details to CompletedTaskDetails
    
    This is a companion PR to this PR: https://github.com/RedHatInsights/tasks-frontend/pull/126
    
    If you follow the guide to run a task with parameters, then you can use this PR to check that the parameters are shown in the completed task details page. Otherwise, you can simply find a completed task that already exists and use that.
    
    To test:
    - Go to `/insights/tasks`
    - Click the 'Activity' tab
    - In the search bar, type 'parameter'. This should show some existing tasks that have run with parameters
    - Click the task to open the completed task details page
    - Check under the description of the task to see the 'Parameter details' section
    
    Do this same thing with a non-parameter task and make sure that 'Parameter details' do NOT appear.
    
    ---------
    
    Co-authored-by: Michael Johnson <micjohns@newbeginnings.usersys.redhat.com>

---
+++ src/SmartComponents/CompletedTaskDetails/ParameterDetails.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import propTypes from 'prop-types';
+import {
+  Flex,
+  FlexItem,
+  Text,
+  TextContent,
+  TextVariants,
+} from '@patternfly/react-core';
+
+const ParameterDetails = ({ parameters }) => {
+  const parametersExist = parameters?.length ? true : false;
+
+  return (
+    parametersExist && (
+      <Flex>
+        <FlexItem>
+          <TextContent>
+            <Text component={TextVariants.h4}>Parameter details</Text>
+            {parameters.map((parameter) => (
+              <Text
+                key={`parameter-${parameter.key}`}
+                component={TextVariants.p}
+                className="pf-u-mb-sm"
+              >
+                {parameter.key}: <b>{parameter.value}</b>
+              </Text>
+            ))}
+          </TextContent>
+        </FlexItem>
+      </Flex>
+    )
+  );
+};
+
+ParameterDetails.propTypes = {
+  parameters: propTypes.array,
+};
+
+export default ParameterDetails;

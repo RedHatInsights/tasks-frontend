061c777d896a962a132146be2dff7b866fd33bf1
Mark Huth <mhuth@redhat.com>
2024-01-20 03:31:11 +1000

    fix(RHINENG-7678): Include task name in Task name filter (#141)

--- src/SmartComponents/ActivityTable/Filters.js
+++ src/SmartComponents/ActivityTable/Filters.js
@@ -5,8 +5,10 @@ export const nameFilter = [
     type: conditionalFilterType.text,
     label: 'Task',
     filter: (tasks, value) =>
-      tasks.filter((task) =>
-        task.task_title.toLowerCase().includes(value.toLowerCase())
+      tasks.filter(
+        (task) =>
+          task.task_title.toLowerCase().includes(value.toLowerCase()) ||
+          task.name.toLowerCase().includes(value.toLowerCase())
       ),
   },
 ];

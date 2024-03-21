e9548bb6108b97626b16d56ff3610253a2a3307c
Mark Huth <mhuth@redhat.com>
2024-03-21 20:32:47 +1000

    feat(RHINENG-2266): Add more task status states & informative messages
    - add job counts to popover body content

--- src/SmartComponents/ActivityTable/Columns.js
+++ src/SmartComponents/ActivityTable/Columns.js
@@ -1,8 +1,20 @@
+import './Column.scss';
 import React from 'react';
 import propTypes from 'prop-types';
 import { renderColumnComponent } from '../../Utilities/helpers';
-import { Icon } from '@patternfly/react-core';
-import { CheckCircleIcon, InProgressIcon } from '@patternfly/react-icons';
+import { TASK_STATUS } from '../../constants';
+import {
+  Icon,
+  Popover,
+  TextContent,
+  TextList,
+  TextListItem,
+} from '@patternfly/react-core';
+import {
+  CheckCircleIcon,
+  ExclamationCircleIcon,
+  InProgressIcon,
+} from '@patternfly/react-icons';
 import InsightsLink from '@redhat-cloud-services/frontend-components/InsightsLink';
 
 const TaskNameCell = ({ id, name }, index) => (
@@ -17,25 +29,117 @@ TaskNameCell.propTypes = {
   index: propTypes.number,
 };
 
-const StatusCell = ({ status }) =>
-  ({
-    Completed: (
-      <span style={{ color: '#3E8635', display: 'flex', alignItems: 'center' }}>
-        <Icon status="success" style={{ marginRight: '4px' }}>
-          <CheckCircleIcon />
-        </Icon>
-        {status}
-      </span>
-    ),
-    Running: (
-      <span style={{ color: '#2B9AF3', display: 'flex', alignItems: 'center' }}>
-        <Icon status="info" style={{ marginRight: '4px' }}>
-          <InProgressIcon />
-        </Icon>
-        {status}
-      </span>
-    ),
-  }[status] || status);
+const getStatusProps = (task) => {
+  let badgeColor, iconStatus, IconType, message, hasPopover;
+
+  message = '';
+  hasPopover = true;
+  switch (task.status) {
+    case TASK_STATUS.RUNNING:
+      badgeColor = '#2B9AF3';
+      iconStatus = 'info';
+      IconType = InProgressIcon;
+      message = [
+        `${task.running_job_count || 0} Running`,
+        `${task.completed_job_count || 0} Completed`,
+      ];
+      break;
+    case TASK_STATUS.COMPLETED:
+      badgeColor = '#3E8635';
+      iconStatus = 'success';
+      IconType = CheckCircleIcon;
+      hasPopover = false;
+      break;
+    case TASK_STATUS.COMPLETED_WITH_ERRORS:
+      badgeColor = '#FF0000';
+      iconStatus = 'danger';
+      IconType = ExclamationCircleIcon;
+      message = [
+        `${task.failure_job_count || 0} Failed`,
+        `${task.timeout_job_count || 0} Timeout`,
+        `${task.completed_job_count || 0} Completed`,
+      ];
+      break;
+    case TASK_STATUS.FAILURE:
+      badgeColor = '#FF0000';
+      iconStatus = 'danger';
+      IconType = ExclamationCircleIcon;
+      message = 'All jobs in this task failed to run on their target systems.';
+      break;
+    case TASK_STATUS.CANCELLED:
+      badgeColor = '#FF0000';
+      iconStatus = 'danger';
+      IconType = ExclamationCircleIcon;
+      message = 'All jobs in this task have been cancelled';
+      break;
+  }
+
+  return { badgeColor, iconStatus, IconType, message, hasPopover };
+};
+
+const StatusCell = (task) => {
+  if (typeof task.status !== 'string') {
+    return;
+  }
+
+  const badgeStyle = {
+    display: 'flex',
+    alignItems: 'center',
+    marginRight: '4px',
+  };
+
+  const { badgeColor, iconStatus, IconType, message, hasPopover } =
+    getStatusProps(task);
+
+  const coloredBadgeStyle = { ...badgeStyle, color: badgeColor };
+  const statusContent = (
+    <span>
+      <Icon status={iconStatus} style={{ marginRight: '4px' }}>
+        <IconType />
+      </Icon>
+      {!hasPopover ? (
+        <span>{task.status}</span>
+      ) : (
+        <span className="popoverLink">{task.status}</span>
+      )}
+    </span>
+  );
+
+  if (!hasPopover) {
+    return <span style={coloredBadgeStyle}>{statusContent}</span>;
+  }
+  let textItemMessage;
+  if (typeof message === 'string') {
+    textItemMessage = <TextListItem>{message}</TextListItem>;
+  } else {
+    textItemMessage = message.map((item, index) => (
+      <TextListItem key={index}>{item}</TextListItem>
+    ));
+  }
+  return (
+    <span style={coloredBadgeStyle}>
+      <Popover
+        style={coloredBadgeStyle}
+        position={'right'}
+        headerIcon={
+          <Icon status={iconStatus}>
+            <IconType />
+          </Icon>
+        }
+        headerContent={<div>{task.status}</div>}
+        bodyContent={
+          <div style={{ color: 'black' }}>
+            <TextContent>
+              <TextList isPlain={true}>{textItemMessage}</TextList>
+            </TextContent>
+          </div>
+        }
+      >
+        {statusContent}
+      </Popover>
+    </span>
+  );
+};
 
 StatusCell.propTypes = {
   status: propTypes.oneOfType([propTypes.string, propTypes.object]),

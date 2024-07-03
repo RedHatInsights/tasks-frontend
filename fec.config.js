a7673f840c5333c8a3c75aafc1e731db4dbcf77d
Muslimjon <mkholjur@redhat.com>
2024-07-03 12:13:02 +0200

    chore(RHINENG-7054): adopt fec.config.js and tsconfig

---
+++ fec.config.js
@@ -0,0 +1,28 @@
+const { resolve } = require('path');
+
+module.exports = {
+  appUrl: '/insights/tasks',
+  debug: true,
+  useProxy: process.env.PROXY === 'true',
+  proxyVerbose: true,
+  plugins: [],
+  ...(process.env.HOT
+    ? { hotReload: process.env.HOT === 'true' }
+    : { hotReload: true }),
+  ...(process.env.port ? { port: parseInt(process.env.port) } : {}),
+  moduleFederation: {
+    shared: [
+      {
+        'react-router-dom': {
+          singleton: true,
+          import: false,
+          version: '^6.8.1',
+          requiredVersion: '>=6.0.0 <7.0.0',
+        },
+      },
+    ],
+    exposes: {
+      './RootApp': resolve(__dirname, './src/AppEntry'),
+    },
+  },
+};

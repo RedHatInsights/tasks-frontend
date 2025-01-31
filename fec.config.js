5f3c1fd67a7f86eb495cf2c7e7b8c21b7c200ec0
Adonis Puente <60629070+adonispuente@users.noreply.github.com>
2025-01-31 16:16:58 -0600

    feat(RHINENG-15388): Onboard tasks to Sentry Instance (#336)

--- fec.config.js
+++ fec.config.js
@@ -1,4 +1,5 @@
 const { resolve } = require('path');
+const { sentryWebpackPlugin } = require('@sentry/webpack-plugin');
 const packageJson = require('./package.json');
 
 module.exports = {
@@ -7,6 +8,27 @@ module.exports = {
   proxyVerbose: true,
   hotReload: process.env.HOT === 'true',
   ...(process.env.port ? { port: parseInt(process.env.port) } : {}),
+  devtool: 'hidden-source-map',
+  plugins: [
+    // Put the Sentry Webpack plugin after all other plugins
+    ...(process.env.ENABLE_SENTRY
+      ? [
+          sentryWebpackPlugin({
+            ...(process.env.SENTRY_AUTH_TOKEN && {
+              authToken: process.env.SENTRY_AUTH_TOKEN,
+            }),
+            org: 'red-hat-it',
+            project: 'tasks-rhel',
+            moduleMetadata: ({ release }) => ({
+              dsn: `https://f8eb44de949e487e853185c09340f3cf@o490301.ingest.us.sentry.io/4505397435367424`,
+              org: 'red-hat-it',
+              project: 'tasks-rhel',
+              release,
+            }),
+          }),
+        ]
+      : []),
+  ],
   moduleFederation: {
     shared: [
       {
